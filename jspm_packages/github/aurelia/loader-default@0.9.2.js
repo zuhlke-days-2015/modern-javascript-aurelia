7f9caa876c562f719fbe2a3c1420e4889e5fd9fc
Stefan Jäger <stefanjaeger@bluewin.ch>
2015-08-05 15:41:18 +0200

    updates

---
+++ jspm_packages/github/aurelia/loader-default@0.9.2.js
@@ -0,0 +1,3 @@
+define(["github:aurelia/loader-default@0.9.2/aurelia-loader-default"], function(main) {
+  return main;
+});
---
+++ jspm_packages/github/aurelia/validation@0.2.7.js
@@ -0,0 +1,3 @@
+define(["github:aurelia/validation@0.2.7/index"], function(main) {
+  return main;
+});
